94801c9342db5a1df9768d29cf7aedfaf6bf0ce6
melvirus <antonio.castillo36@gmail.com>
2023-01-17 00:56:19 -0600

    agregando funcion descodificar

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 function codificar(cadena) {
-
   return btoa(cadena);
-
 }
-  
 
 function descodificar(cadena) {
-
   return atob(cadena);
-
 }
-  
 
 let codificarBtn = document.getElementById("codificar");
-
 let descodificarBtn = document.getElementById("descodificar");
-
 let mensaje = document.getElementById("mensaje");
-  
 
 codificarBtn.addEventListener("click", function(event) {
   event.preventDefault();
   mensaje.value = codificar(mensaje.value);
-}    
+});
+
+descodificarBtn.addEventListener("click", function(event) {
+event.preventDefault();
+mensaje.value = descodificar(mensaje.value);
+});
